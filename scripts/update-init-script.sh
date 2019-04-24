6aa1f06fccaea0cfe9ce04dacc07e4329904d069
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2019-04-24 13:50:22 +0800

    More changes
    Move pt-os-upgrader into squashfs
    No longer check for files in /boot
    Add shutdown and developer options on error
    Ask user to provide upgrade log if they contact support
    Print config file contents in verbose mode

--- scripts/update-init-script.sh
+++ scripts/update-init-script.sh
@@ -2,12 +2,15 @@
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+src_dir="$(dirname ${script_dir})/src"
+
 rm -rf /tmp/squashfs/ 2>/dev/null
 mkdir -p /tmp/squashfs/ 2>/dev/null
 
 unsquashfs -f -d /tmp/squashfs/ "${1}"
 
-cp "${script_dir}/init" /tmp/squashfs/init
+cp "${src_dir}/init" /tmp/squashfs/init
+cp "${src_dir}/pt-os-upgrader" /tmp/squashfs/pt-os-upgrader
 
 rm "${1}"
 mksquashfs /tmp/squashfs/ "${1}"
